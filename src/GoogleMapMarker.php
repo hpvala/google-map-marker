a3d8416f028079b2b42d6151f6a765bf85023eac
Hitesh <hitesh.v@samcomtechnobrains.com>
2023-05-08 10:59:42 +0530

    Add address method

--- src/GoogleMapMarker.php
+++ src/GoogleMapMarker.php
@@ -13,13 +13,37 @@ class GoogleMapMarker
         $this->apiKey = $apiKey;
     }
 
-    public function getLocationDetails($lat, $lng)
+    /**
+     * Get details of locations based on latitude and longitude
+     *
+     * @param float $lat
+     * @param float $lng
+     * @return array|null
+     */
+    public function getLocationDetails($lat, $lng): ?array
     {
         $client = new Client();
 
         $response = $client->get("https://maps.googleapis.com/maps/api/geocode/json?latlng={$lat},{$lng}&key={$this->apiKey}");
 
-        $result = json_decode($response->getBody()->getContents(), true);
+        $result = json_decode($response->getBody()->getContents(), true) ?? null;
+
+        return $result;
+    }
+
+    /**
+     * Get details of full address based on address
+     *
+     * @param string $address
+     * @return array|null
+     */
+    public function getAddressDetails($address): ?array
+    {
+        $client = new Client();
+
+        $response = $client->get("https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key={$this->apiKey}");
+
+        $result = json_decode($response->getBody()->getContents(), true) ?? null;
 
         return $result;
     }
